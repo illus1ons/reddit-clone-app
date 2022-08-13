6c4f3ed30bd652e335858be0ea712f6ec3e85467
Youngbae Yoo <youngbae.u@gmail.com>
2022-08-14 01:14:55 +0900

    Add register page

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -23,17 +23,14 @@ function InputGroup({
             <input
                 type={type}
                 style={{ minWidth: 300 }}
-                className={cls(
-                    `w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
-                    {
-                        "border-red-500": error
-                    }
+                className={cls(`w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white text-gray-400`,
+                    { "border-red-500": error }
                 )}
                 placeholder={placeholder}
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
             />
-            <small className="font-medium text-red-500">{error}</small>
+            <small className='font-medium text-red-500'>{error} </small>
         </div>
     );
 }
