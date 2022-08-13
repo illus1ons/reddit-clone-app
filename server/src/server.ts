2fbeb25a0017d948b88356d6b52ea16df3b08115
Youngbae Yoo <youngbae.u@gmail.com>
2022-08-14 01:46:44 +0900

    Add auth router

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { AppDataSource } from "./data-source"
+import authRoutes from "./routes/auth";
 
 const app = express();
 const port = 4000;
+const origin = "http://localhost:3000";
 
+app.use(cors({
+    origin
+}))
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (_, res) => res.send("runngin"));
+app.use("/api/auth", authRoutes);
 
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
